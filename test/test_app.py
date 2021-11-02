170d3365ccf43b03668533df01bda677cfd86d67
Diane Zhang <yizhan22@colby.edu>
2021-11-02 11:13:14 -0400

    Updated test file

--- test/test_app.py
+++ test/test_app.py
@@ -1,5 +1,12 @@
-from app import about
+from app import *
+import requests
+
+website_path = "https://calm-ravine-03448.herokuapp.com/"
 
 def test_about():
     
     assert about() == "TODO by Diane and Hoang"
+    
+    clinet = app.test_client()
+    response = client.get("/about")
+    assert response.status_code == 200 #success
