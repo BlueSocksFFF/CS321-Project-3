9c49c43379c3c9fa57cb4cc472d056941488ea0b
Diane Zhang <yizhan22@colby.edu>
2021-11-01 22:03:59 -0400

    deleted code unneeded

--- app.py
+++ app.py
@@ -41,27 +41,5 @@ def view_index():
         create_item(request.form['text'])
     return render_template("base.html", items = read_items())
 
-@app.route("/about")
-def about():
-	return "A todo list created by Diane and Hoang"
-
-@app.route("/add", methods=["POST"])
-def add():
-	todo_item = request.form.get("todo")
-	todo_list.append(todo_item)
-
-	return redirect(url_for("index"))
-
-@app.route("/remove/<string:item>")
-def remove(item):
-	todo_list.remove(item)
-	return redirect(url_for("index"))
-
-@app.route("/complete/<string:item>")
-def complete(item):
-    accomplished_list.append(item)
-    todo_list.remove(item)
-    return redirect(url_for("index"))
-
 if __name__ == "__main__":
 	app.run(debug=True)
