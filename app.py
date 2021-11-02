9dd0ee5ee74ad237a00795d6ad336d2cd6f68248
Diane Zhang <yizhan22@colby.edu>
2021-11-01 21:50:24 -0400

    Added item class and SQL database

--- app.py
+++ app.py
@@ -1,9 +1,37 @@
+from typing_extensions import NotRequired
 from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 
-todo_list = []
-accomplished_list = []
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement = True)
+    text = db.Column(db.Text)
+    priority = db.Column(db.Enum)
+    done = db.Column(db.Boolean)
+    dateTime = db.Column(db.DateTime, default=datetime.now())
+    
+    def create_item(text):
+        item = Item(text = text)
+        db.session.add(item)
+        db.session.commit()
+        db.session.refresh(item)
+        
+    def read_items():
+        return db.session.query(Item)
+    
+    def update_item(item_id, text, done):
+        db.session.query(Item).filter_by(id = item_id).update({
+			"text": text,
+			"done": done #maybe update later
+		})
+        db.session.commit()
+        
+    def delete_item(item_id):
+        db.session.query(Item).filter_by(id=item_id).delete()
+        db.session.commit()
 
 @app.route("/")
 def index():
