76777defbc8181875cfa3ed2176bd4db8b76c7b0
Diane Zhang <yizhan22@colby.edu>
2021-11-01 22:02:51 -0400

    First version of website

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from datetime import datetime
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
+# class and methods
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement = True)
     text = db.Column(db.Text)
@@ -13,29 +14,32 @@ class Item(db.Model):
     done = db.Column(db.Boolean)
     dateTime = db.Column(db.DateTime, default=datetime.now())
     
-    def create_item(text):
-        item = Item(text = text)
-        db.session.add(item)
-        db.session.commit()
-        db.session.refresh(item)
+def create_item(text):
+    item = Item(text = text)
+    db.session.add(item)
+    db.session.commit()
+    db.session.refresh(item)
         
-    def read_items():
-        return db.session.query(Item)
+def read_items():
+    return db.session.query(Item)
     
-    def update_item(item_id, text, done):
-        db.session.query(Item).filter_by(id = item_id).update({
-			"text": text,
-			"done": done #maybe update later
-		})
-        db.session.commit()
+def update_item(item_id, text, done):
+    db.session.query(Item).filter_by(id = item_id).update({
+		"text": text,
+		"done": True if done == "on" else False #checkbox
+	})
+    db.session.commit()
         
-    def delete_item(item_id):
-        db.session.query(Item).filter_by(id=item_id).delete()
-        db.session.commit()
+def delete_item(item_id):
+    db.session.query(Item).filter_by(id=item_id).delete()
+    db.session.commit()
 
-@app.route("/")
-def index():
-	return render_template("base.html", title="home", todo_list=todo_list, accomplished_list = accomplished_list)
+# app
+@app.route("/", methods = ["POST", "GET"])
+def view_index():
+    if request.method == "POST":
+        create_item(request.form['text'])
+    return render_template("base.html", items = read_items())
 
 @app.route("/about")
 def about():
