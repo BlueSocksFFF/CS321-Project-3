cec52e076c39ebc3684775e7c090c096b9d0ec02
Diane Zhang <yizhan22@colby.edu>
2021-11-10 04:23:59 -0500

    pytest

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import os
 import enum
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = "sqlite:///{}".format(os.path.join(project_dir, "todo.db"))
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
--- test_app.py
+++ test_app.py
@@ -1,5 +1,4 @@
 from app import *
-import requests
 
 website_path = "https://calm-ravine-03448.herokuapp.com/"
 
@@ -15,5 +14,55 @@ def test_view_index():
     
     client = app.test_client()
     url = "/"
-    response = client.post(text="test_todo", priority="aHIGH")
+    response = client.post()
+    assert response.status_code == 400 #bad request
+    
+    response = client.post(url, data="test")
+    assert response.status_code == 400
+    
+    text_data = {"text": "Test"}
+    response = client.post(url, data=text_data)
     assert response.status_code == 200
+    assert b'Test' in response.data
+    
+    priority_data = {"priority": "aHIGH"}
+    response = client.post(url, data=priority_data)
+    assert response.status_code == 400 #why?
+    
+    response = client.get("/")
+    assert response.status_code == 200
+    
+def test_edit_item():
+    client = app.test_client()
+    url = "/edit/0"
+    
+    text_data = {"text": "Test"}
+    client.post("/", data=text_data)
+    
+    response = client.get(url)
+    assert response.status_code == 302 #Success defined in method
+    assert b'Test' not in response.data
+    
+def test_delete():
+    client = app.test_client()
+    url = "/delete/0"
+    
+    text_data = {"text": "Test"}
+    response = client.post("/", data=text_data)
+    
+    response = client.post(url)
+    assert response.status_code == 303 #Success defined in method
+    assert b'Test' not in response.data
+    
+def test_edit_priority():
+    client = app.test_client()
+    
+    url = "/priority/0"
+    data = {"priority":"HIGH"}
+    
+    text_data = {"text": "Test"}
+    response = client.post("/", data=text_data)
+    
+    response = client.post(url) # Not working
+    assert response.status_code == 304
+    
